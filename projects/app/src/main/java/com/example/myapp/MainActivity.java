42a16e55fdb65af46e4de0c62e03f252d95d5351
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-02-15 12:41:06 +0300

    add projects

---
+++ projects/app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText text;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        text = findViewById(R.id.editTextText);
+        textView = findViewById(R.id.textView);
+        btn.setOnClickListener((View view)-> textView.setText(text.getText().toString()));
+    }
+}
