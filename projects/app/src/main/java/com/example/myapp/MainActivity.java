e31875df8dca5ffa33629e587515881d82c1dd8c
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-02-15 13:04:02 +0300

    simple calculator

--- projects/app/src/main/java/com/example/myapp/MainActivity.java
+++ projects/app/src/main/java/com/example/myapp/MainActivity.java
@@ -13,16 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
     EditText text;
+    EditText text2;
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.button);
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
         text = findViewById(R.id.editTextText);
+        text2 = findViewById(R.id.editTextText2);
         textView = findViewById(R.id.textView);
-        btn.setOnClickListener((View view)-> textView.setText(text.getText().toString()));
+        btn1.setOnClickListener((View view)-> {
+            int a = Integer.parseInt(text.getText().toString())+Integer.parseInt(text2.getText().toString());
+            textView.setText(String.valueOf(a));});
+        btn2.setOnClickListener((View view)-> {
+            int a = Integer.parseInt(text.getText().toString())-Integer.parseInt(text2.getText().toString());
+            textView.setText(String.valueOf(a));});
+        btn3.setOnClickListener((View view)-> {
+            int a = Integer.parseInt(text.getText().toString())*Integer.parseInt(text2.getText().toString());
+            textView.setText(String.valueOf(a));});
+        btn4.setOnClickListener((View view)-> {
+            int a = Integer.parseInt(text.getText().toString())/Integer.parseInt(text2.getText().toString());
+            textView.setText(String.valueOf(a));});
     }
 }
