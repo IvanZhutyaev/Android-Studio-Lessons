8f5678e30d6e3398cb8a1b6da8d78215b037fce9
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-02-15 14:32:16 +0300

    new activity

---
+++ projects/app/src/main/java/com/example/myapp/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapp.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+    ActivitySecondBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
