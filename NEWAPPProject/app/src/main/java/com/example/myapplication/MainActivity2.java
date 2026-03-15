cc761767a069741ccacccd1379e99e7acf750cc1
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-03-15 14:24:08 +0300

    update

--- NEWAPPProject/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ NEWAPPProject/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -15,13 +15,13 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity2 extends AppCompatActivity {
     ActivityMain2Binding binding;
-
+    Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         binding= ActivityMain2Binding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
-
+        getBundle(bundle);
         binding.prevBtn.setOnClickListener(c->{
             Intent intent = new Intent(MainActivity2.this, MainActivity.class);
             startActivity(intent);
@@ -29,5 +29,13 @@ public class MainActivity2 extends AppCompatActivity {
         binding.nextBtn.setOnClickListener(c->{
             Toast.makeText(this, "Ne zmay", Toast.LENGTH_LONG).show();
         });
+    }
+
+    private void getBundle(Bundle bundle){
+        bundle = getIntent().getExtras();
+        if(bundle!=null){
+            binding.nameEdt.setText(bundle.getString("name"));
+            binding.cityEdt.setText(bundle.getString("city"));
+        }
     }
 }
